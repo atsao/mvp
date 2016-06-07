36995ffd71069135d456daa47e6bea4725680874
Andrew Tsao <andrew.tsao8@gmail.com>
2016-06-06 19:59:04 -0700

    Base application functionality working

--- client/app.js
+++ client/app.js
@@ -22,37 +22,113 @@ pickpal.config(function($stateProvider, $urlRouterProvider) {
     })
     .state('pick.choose', {
       url: '/choose',
-      templateUrl: 'views/pick-choose.html'
+      templateUrl: 'views/pick-choose.html',
+      // resolve: {
+      //   dataSuccess: ['$q', 'Yelp', function($q, Yelp) {
+      //     var deferred = $q.defer();
+      //     if (Yelp.dataSuccess) {
+      //       deferred.resolve();
+      //     } else {
+      //       deferred.reject('Data not received');
+      //     }
+
+      //     return deferred.promise;
+      //   }]
+      // }
+      resolve: {
+        dataSuccess: function(Yelp) {
+          return Yelp.getData();
+        }
+      }
     });
 
   // $locationProvider.html5Mode(true);
   $urlRouterProvider.otherwise('pick');
 });
 
-pickpal.controller('pickController', ['$scope', 'Yelp', function($scope, Yelp) {
+// pickpal.run(function($rootScope, $state, $urlRouter, Yelp) {
+//   $rootScope.$on('$stateChangeStart', function(event, toState, toParams, fromState, fromParams, options) {
+//     console.log('event: ', event);
+//     console.log('toState:', toState);
+//     console.log('fromState: ', fromState);
+
+//     if (toState.name === 'choose' && fromState.name === 'mood' && Yelp.dataSuccess === false) {
+//       event.preventDefault();
+//     } else {
+//       console.log('test!');
+//     }
+//   });
+// });
+
+pickpal.controller('pickController', ['$scope', 'Yelp', '$q', function($scope, Yelp, $q) {
   $scope.pickData = {};
 
   $scope.processForm = function() {
-    console.log('inside processForm');
-    console.log($scope.pickData);
-    Yelp.getData($scope.pickData);
+    // var deferred = $q.defer();
+    // return Yelp.getData($scope.pickData).then(function(data) {
+    //   console.log('success!: ');
+    //   // return data;
+    //   console.log('data!: ', data);
+    //   deferred.resolve(data);
+    // }).catch(function(error) {
+    //   console.log('error!!!!!!!!: ');
+    //   deferred.reject('error bish');
+    // });
+
+    // return deferred.promise;
+    // $scope.data = JSON.stringify(Yelp.getData());
+    return Yelp.getData($scope.pickData).then(function(data) {
+      console.log('retrieving data...');
+      $scope.data = data;
+    }, function(error) {
+      console.error(error);
+    })
   }
 }]);
 
 // // SERVICES
-pickpal.factory('Yelp', function($http) {
+pickpal.factory('Yelp', function(YelpService, $http) {
   return {
+    // dataSuccess: false,
+
+    // getData: function(pickData) {
+    //   console.log('whaddup bitch');
+    //   return $http({
+    //     method: 'GET',
+    //     url: '/api/pick',
+    //     data: pickData
+    //   }).then(function(data) {
+    //     console.log('factory data:', data);
+    //     // this.dataSuccess = true;
+    //     // console.log('this.data: ', this.dataSuccess);
+    //     return data;
+    //   }, function(error) {
+    //     console.error(error);
+    //     return error;
+    //   });
+    // }
+
     getData: function(pickData) {
-      console.log('whaddup bitch');
-      $http({
+      return YelpService.requestData($http({
         method: 'GET',
         url: '/api/pick',
         data: pickData
-      }).then(function(data) {
-        console.log('factory data:', data);
-      }, function(error) {
-        console.error(error);
-      })
+      }));
+    }
+  }
+});
+
+pickpal.service('YelpService', function($q) {
+  return {
+    requestData: function(httpPromise) {
+      var deferred = $q.defer();
+      httpPromise.then(function(data) {
+        deferred.resolve(data);
+      }, function() {
+        deferred.reject('No data retrieved');
+      });
+
+      return deferred.promise;
     }
   }
 });
