ad71e8ed879a5af817a9085d8af319e38d4e52e9
Andrew Tsao <andrew.tsao8@gmail.com>
2016-07-29 10:57:50 -0700

    (dev) Adjusts environment variables

--- env.js
+++ env.js
@@ -13,9 +13,9 @@
 // Deployed - use environment variables
 module.exports = {
   yelp: {
-    consumer_key: process.env.YELP.CONSUMER_KEY,
-    consumer_secret: process.env.YELP.CONSUMER_SECRET,
-    token: process.env.YELP.TOKEN,
-    token_secret: process.env.YELP.TOKEN_SECRET,
+    consumer_key: process.env.YELP_CONSUMER_KEY,
+    consumer_secret: process.env.YELP_CONSUMER_SECRET,
+    token: process.env.YELP_TOKEN,
+    token_secret: process.env.YELP_TOKEN_SECRET,
   }
 }
